1ea6b9a2565723088b91947bb1b5189879a28ed2
wtf-sys <52872931+wtf-sys@users.noreply.github.com>
2026-06-03 21:06:28 -0700

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int slen = s.length();
+        set<char> hs;
+
+        if (slen == 1) {
+            return 1;
+        }
+        if (slen == 0) {
+            return 0;
+        }
+
+        hs.insert(s[0]);
+
+        int r = 1;
+        int l = 0;
+        int maxlen = 0;
+
+        while(r < slen) {
+            if (hs.contains(s[r])) {
+                hs.erase(s[l]);
+                l++;
+            } else {
+                hs.insert(s[r]);
+                r++;
+            }
+            int curlen = hs.size();
+            if (curlen > maxlen) {
+                maxlen = curlen;
+            }
+        }
+
+        return maxlen;
+    }
+};
